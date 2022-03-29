f4219e038cd93c7c96fc058d1fa2103f78321bb0
liquidph34r <57736737+liquidph34r@users.noreply.github.com>
2022-03-29 16:57:21 -0500

    Update Shipping_DB.sql
    Added account table

--- Shipping_DB.sql
+++ Shipping_DB.sql
@@ -17,8 +17,25 @@ CREATE TABLE Packages
      next_distro_id     numeric(4,0),
      out_for_del        boolean,
      status             varchar(15),
+     account_id_ref     numeric(10,0),
      PRIMARY KEY (package_id),
      Foreign key(package_id) REFERENCES trucks, -- THis might get removed. Need to find a way to do trucks
      foreign key(last_distro_id, next_distro_id) REFERENCES distro
      );
      
+CREATE TABLE Account
+    (Account_id                 numeric(10,0),
+     first_name                 varchar(10),
+     last_name                  varchar(10),
+     Middle_init                varchar(1),
+     email                      varchar(20),
+     password                   varchar(20),
+     default_shipping_adress    varchar(15),
+     default_shipping_city      varchar(15),
+     default_shipping_zip       numeric(5,0),
+     default_shipping_country   varchar(15),
+     active_package_ids         varchar(15), -- Needs to be a multiple field, not sure how to do.
+     primary key (account_id),
+     foreign key (account_id) REFERENCES Packages,
+     foreign key (active_package_ids) REFERENCES Packages -- Might get rid of
+     );
